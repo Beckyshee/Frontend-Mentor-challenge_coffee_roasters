fe1f443d4dc8ad6b11ff96b12e9eab2927896042
Beckyshee <wanjirubecky.rw@gmail.com>
2023-11-17 12:21:12 +0300

    footer

--- coffee/src/app/app-routing.module.ts
+++ coffee/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LandingComponent } from './landing/landing.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { PlanComponent } from './plan/plan.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NavbarComponent },
+  { path: 'landing', component: LandingComponent },
+  { path: 'about', component: AboutUsComponent},
+  // { path: 'plan', component: PlanComponent},
+  { path: 'footer', component: FooterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- coffee/src/app/app.module.ts
+++ coffee/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { LandingComponent } from './landing/landing.component';
+import { FooterComponent } from './footer/footer.component';
+import { PlanComponent } from './plan/plan.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+   
+    LandingComponent,
+    FooterComponent,
+    PlanComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
