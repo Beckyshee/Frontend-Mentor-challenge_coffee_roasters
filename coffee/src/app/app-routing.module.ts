98dd6cdd7932a4164bcb7a86f6f314f416af9b9d
Beckyshee <wanjirubecky.rw@gmail.com>
2023-11-19 12:26:47 +0300

    flex cards

--- coffee/src/app/app-routing.module.ts
+++ coffee/src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   { path: '', component: NavbarComponent },
   { path: 'landing', component: LandingComponent },
   { path: 'about', component: AboutUsComponent},
-  // { path: 'plan', component: PlanComponent},
+  { path: 'plan', component: PlanComponent},
   { path: 'footer', component: FooterComponent},
 ];
 
